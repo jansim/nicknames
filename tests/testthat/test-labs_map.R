39d05bf71349c28a9e1c5a45e5f252a436714caf
Jan Simson <git@simson.io>
2025-08-13 20:44:54 +0200

    Add a test case for labs_map with lists

--- tests/testthat/test-labs_map.R
+++ tests/testthat/test-labs_map.R
@@ -161,3 +161,18 @@ test_that("labs_map works with partial mapping and preserves unmapped aesthetics
   expect_equal(p$labels$size, "hp")
   expect_equal(p$labels$shape, "factor(gear)")
 })
+
+test_that("labs_map works with a list", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg)) +
+    ggplot2::geom_point()
+
+  p <- p +
+    labs_map(list(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles/(US) gallon"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles/(US) gallon")
+})
