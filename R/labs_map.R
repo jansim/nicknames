2948414a90a48e6e8ea9e5df4bcdd1009c037893
Jan Simson <git@simson.io>
2025-08-13 20:32:33 +0200

    Add support for function calls in aesthetics + additional test cases

--- R/labs_map.R
+++ R/labs_map.R
@@ -23,7 +23,7 @@ ggplot_add.labs_map <- function(object, plot, object_name) {
   # Retrieve correct names for all aesthetics
   for (var_name in names(object$names)) {
     for (aes_name in names(aes_mapping)) {
-      if (!is.null(aes_mapping[[aes_name]]) && rlang::as_name(aes_mapping[[aes_name]]) == var_name) {
+      if (!is.null(aes_mapping[[aes_name]]) && rlang::as_label(aes_mapping[[aes_name]]) == var_name) {
         labs_args[[aes_name]] <- object$names[[var_name]]
       }
     }
--- tests/testthat/test-labs_map.R
+++ tests/testthat/test-labs_map.R
@@ -12,3 +12,152 @@ test_that("labs_map works with x and y", {
   expect_equal(p$labels$x, "Weight (1000 lbs)")
   expect_equal(p$labels$y, "Miles/(US) gallon")
 })
+
+test_that("labs_map works with nothing matching", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg)) +
+    ggplot2::geom_point()
+
+  p <- p +
+    labs_map(c(
+      not_wt = "Weight (1000 lbs)",
+      not_mpg = "Miles/(US) gallon"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "wt")
+  expect_equal(p$labels$y, "mpg")
+})
+
+
+test_that("labs_map works with empty names", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg)) +
+    ggplot2::geom_point()
+
+  p <- p +
+    labs_map(c())
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "wt")
+  expect_equal(p$labels$y, "mpg")
+})
+
+test_that("labs_map works with color and size aesthetics", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, color = cyl, size = hp)) +
+    ggplot2::geom_point()
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles/(US) gallon",
+      cyl = "Number of cylinders",
+      hp = "Horsepower"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles/(US) gallon")
+  expect_equal(p$labels$colour, "Number of cylinders")
+  expect_equal(p$labels$size, "Horsepower")
+})
+
+test_that("labs_map works with fill and alpha aesthetics", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, fill = factor(vs), alpha = qsec)) +
+    ggplot2::geom_point(shape = 21, size = 3)
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles per gallon",
+      "factor(vs)" = "Engine shape",
+      qsec = "Quarter mile time"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles per gallon")
+  expect_equal(p$labels$fill, "Engine shape")
+  expect_equal(p$labels$alpha, "Quarter mile time")
+})
+
+test_that("labs_map works with shape and linetype aesthetics", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, shape = factor(gear), linetype = factor(am))) +
+    ggplot2::geom_point(size = 3) +
+    ggplot2::geom_line()
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles per gallon",
+      "factor(gear)" = "Number of gears",
+      "factor(am)" = "Transmission type"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles per gallon")
+  expect_equal(p$labels$shape, "Number of gears")
+  expect_equal(p$labels$linetype, "Transmission type")
+})
+
+test_that("labs_map works with stroke and group aesthetics", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, stroke = disp, group = factor(carb))) +
+    ggplot2::geom_point(shape = 21, size = 3) +
+    ggplot2::geom_line()
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles per gallon",
+      disp = "Engine displacement",
+      "factor(carb)" = "Number of carburetors"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles per gallon")
+  expect_equal(p$labels$stroke, "Engine displacement")
+  expect_equal(p$labels$group, "Number of carburetors")
+})
+
+test_that("labs_map works with multiple aesthetics including facets", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, color = hp, size = drat)) +
+    ggplot2::geom_point() +
+    ggplot2::facet_grid(vs ~ am)
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles per gallon",
+      hp = "Gross horsepower",
+      drat = "Rear axle ratio",
+      vs = "Engine (0 = V-shaped, 1 = straight)",
+      am = "Transmission (0 = automatic, 1 = manual)"
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles per gallon")
+  expect_equal(p$labels$colour, "Gross horsepower")
+  expect_equal(p$labels$size, "Rear axle ratio")
+})
+
+test_that("labs_map works with partial mapping and preserves unmapped aesthetics", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg, color = cyl, size = hp, shape = factor(gear))) +
+    ggplot2::geom_point()
+
+  p <- p +
+    labs_map(c(
+      wt = "Weight (1000 lbs)",
+      mpg = "Miles per gallon",
+      cyl = "Number of cylinders"
+      # hp and gear are intentionally not mapped
+    ))
+
+  built_plot <- ggplot2::ggplot_build(p)
+  expect_equal(p$labels$x, "Weight (1000 lbs)")
+  expect_equal(p$labels$y, "Miles per gallon")
+  expect_equal(p$labels$colour, "Number of cylinders")
+  # These should remain as original variable names
+  expect_equal(p$labels$size, "hp")
+  expect_equal(p$labels$shape, "factor(gear)")
+})
